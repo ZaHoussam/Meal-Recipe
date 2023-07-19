5abd4b1ea95eed41e294d240a1954802a41f5ad2
ZaHoussam <89077423+ZaHoussam@users.noreply.github.com>
2023-07-19 20:42:11 +0200

    Meal Recipe Project

---
+++ vue.config.js
@@ -0,0 +1,16 @@
+const { defineConfig } = require("@vue/cli-service");
+module.exports = defineConfig({
+  transpileDependencies: true,
+  configureWebpack: {
+    performance: {
+      hints: false,
+    },
+    optimization: {
+      splitChunks: {
+        minSize: 10000,
+        maxSize: 250000,
+      },
+    },
+  },
+  publicPath: "/Meal-Recipe/",
+});
